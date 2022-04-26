1c01d500200bf703d1b9e9cf2ead4f3f8761104d
pauls toma <paulsttoma@icloud.com>
2022-04-25 22:11:15 -0400

    pauls toma

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from PT
+          Learn React from Pauls Toma 
         </a>
       </header>
     </div>
