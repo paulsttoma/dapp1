7b20e74c402de7eb7ad9d53e6ee2302eaa1fa1dc
pauls toma <paulsttoma@icloud.com>
2022-04-25 22:16:56 -0400

    commit 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+function Header() {
+  return (
+    <div>
+        <Typography> Pauls Toma was here</Typography>
+    </div>
+  )
+}
+
+export default Header
