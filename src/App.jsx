a5c85b853a5d09b129f5e3ff5e794c824bd3760e
pauls toma <paulsttoma@icloud.com>
2022-04-27 18:10:38 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Nav from "./components/Nav";
+import './App.css';
 
 function App() {
   return (
-    <div>
-      
-      <Nav></Nav>
-
+    <div className="App-header">
+      <p> Hi </p>
     </div>
   );
 }
