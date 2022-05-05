ac091fe7faea144ccf4a1d02bf90b2a1b21d89c1
pauls toma <paulsttoma@icloud.com>
2022-05-05 15:23:59 -0400

    git

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <p> Hello World </p>
-      </div>
+    <div className="Container">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+    </div>
   );
 }
 export default App;
