3e5e8bb9f7eaf3d31b1827698f3fd43f0808f787
pauls toma <paulsttoma@icloud.com>
2022-05-09 23:34:37 -0400

    seville

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import "./App.css";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="Container">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-    </div>
-  );
+  return <div className="Container">
+    <div className="gg"> Hello Seville</div>
+  </div>;
 }
 export default App;
