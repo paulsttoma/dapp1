60f2b8cf7de8ebbaf40ae9c5778e0eed548839b2
pauls toma <paulsttoma@icloud.com>
2022-05-19 08:20:46 -0400

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+let Logo = require("../images/ss.png")
 
 
 
@@ -6,13 +7,13 @@ function Navbar() {
   return (
     <div>
       <header>
-          <img className="" src="images/img1.jpg" alt="logo" />
+          <img className="logo" src={Logo} alt="logo" />
         <nav>
-          <ul>
-            <li><a href="www.google.com">1</a></li>
-            <li><a href="www.google.com">2</a></li>
-            <li><a href="www.google.com">3</a></li>
-            <li><a href="www.google.com">4</a></li>
+          <ul className="nav_link">
+            <li><a href="www.google.com">Etherscan</a></li>
+            <li><a href="www.google.com">Twitter</a></li>
+            <li><a href="www.google.com">Whitepaper</a></li>
+            <li><a href="www.google.com">Opensea</a></li>
           </ul>
         </nav>
         <a className="" href="www.google.com"><button>Mint</button></a>
